41de01263dde37cf5ffbaadeddc0145acf1290c7
20Simon17 <simonlandin05@gmail.com>
2025-10-20 18:38:23 +0200

    Added more character movement functionality.
    Added:
    -Player camera
    -Sliding
    -Jumping
    -Ground slam
    
    Fixed:
    -Movement

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -8,28 +8,60 @@ public class CharacterMovement : MonoBehaviour
 {
     private Rigidbody rb;
     private Vector2 movementVector;
+    private Vector3 defaultGravity;
     
-    [SerializeField]
-    private float maxVelocity = 10f;
+    //TODO: convert all of these to properties and set "disableMovement" to true in the setters?
+    [Header("Debugging")] 
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private bool canJump;
+    [SerializeField] private bool isSliding;
+
+    private bool CanJump
+    {
+        get => isGrounded || canJump;
+        set => canJump = value;
+    }
     
-    [SerializeField]
-    private float accelerationForce = 10f;
+    [Header("Ground")]
+    [SerializeField] private float counterForceThreshold = 3.0f;
+    
+    [Header("Air")]
+    [SerializeField] private float airGravityMultiplier = 1.0f;
+
+    [Header("Jump")]
+    [SerializeField] private float jumpForce = 7.5f;
+    
+    [Header("Slide")]
+    [SerializeField] private float slideSpeedBoost = 1.2f;
+    [SerializeField] private float slideSpeed = 9.0f;
+
+    [Header("Ground Slam")]
+    [SerializeField] private float groundSlamForce = 10f;
+    
+    [Header("Movement")]
+    [SerializeField] private float maxVelocity = 10f;
+    [SerializeField] private float accelerationForce = 50f;
+    [SerializeField] private float decelerationForce = 50f;
     
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        defaultGravity = Physics.gravity;
     }
     
     private void FixedUpdate()
     {
-        UpdateMovement();
+        GroundCheck();
+        if (!isSliding) UpdateMovement();
+        UpdateGravity();
     }
-
+    
     private void UpdateMovement()
     {
-        if (movementVector == Vector2.zero)
+        if (movementVector == Vector2.zero && rb.linearVelocity.magnitude > counterForceThreshold)
         {
-            
+            Vector3 counterForce = new Vector3(-rb.linearVelocity.x, 0, -rb.linearVelocity.z).normalized;
+            rb.AddForce(counterForce * decelerationForce);
         }
         else if (rb.linearVelocity.magnitude < maxVelocity)
         {
@@ -37,9 +69,104 @@ public class CharacterMovement : MonoBehaviour
             rb.AddForce(transform.right * (movementVector.x * accelerationForce));
         }
     }
+
+    private void GroundCheck()
+    {
+        Ray groundRay = new Ray(transform.position, Vector3.down);
+        if (Physics.Raycast(groundRay, out RaycastHit hitInfo, 1.1f))
+        {
+            if (hitInfo.collider.CompareTag("Ground"))
+            {
+                isGrounded = true;
+            }
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void UpdateGravity()
+    {
+        if (!isGrounded && rb.linearVelocity.y < 0)
+        {
+            Physics.gravity = defaultGravity * airGravityMultiplier;
+        }
+        else
+        {
+            Physics.gravity = defaultGravity;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
     
     private void OnMove(InputValue value)
     {
         movementVector = value.Get<Vector2>();
+    }
+    
+    private void OnJump(InputValue value)
+    {
+        if (value.isPressed && CanJump)
+        {
+            rb.AddForce(transform.up * jumpForce * 100, ForceMode.Force);
+            CanJump = false;
+        }
+    }
+
+    private void OnCrouch(InputValue value)  //TODO: slide towards players forward or based on movement input?
+    {
+        //TODO: probably need to implement a slide cooldown to prevent spamming and infinite speed
+        if (value.isPressed)
+        {
+            isSliding = true;
+            //add physics material with ~0.05 friction to player collider - or potentially edit the properties of the existing physics material
+
+            if (!isGrounded) return;
+            
+            if (rb.linearVelocity.magnitude < slideSpeed)
+            {
+                rb.linearVelocity = transform.forward * slideSpeed;
+            }
+            else
+            {
+                rb.linearVelocity = transform.forward * rb.linearVelocity.magnitude * slideSpeedBoost;
+            }
+        }
+        else
+        {
+            isSliding = false;
+            //remove physics material from player collider
+        }
+    }
+    
+    private void OnSlam(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.AddForce(-transform.up * groundSlamForce * 100, ForceMode.Force);
+        }
+    }
+    
+    private void OnInteract(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            CanJump = true;
+        }
     }
 }
--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerCamera : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject playerObject;
+    
+    [SerializeField]
+    private float mouseSensitivity = 1f;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] 
+    private float clampAngleMin = -45f;
+    
+    [SerializeField] 
+    private float clampAngleMax = 45f;
+
+    private float cameraVerticalRotation = 0f;
+    
+    private void Awake()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         
+        if (playerObject == null)
+        {
+            playerObject = transform.parent.gameObject;
+        }
+        
+        transform.rotation = playerObject.transform.rotation;
+    }
+    
+    private void OnLook(InputValue value)
+    {
+        Vector2 lookVector = value.Get<Vector2>();
+        playerObject.transform.Rotate(Vector3.up, lookVector.x * mouseSensitivity);
+        
+        cameraVerticalRotation += -lookVector.y * mouseSensitivity;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, clampAngleMin, clampAngleMax);
+        transform.localEulerAngles = new Vector3(cameraVerticalRotation, 0f, 0f);
     }
 }
